8ec4353563938872fa487a0189ecf5874443d60a
Prakash kumar singh <Pk87096639@gmail.com>
2024-09-21 15:46:41 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarlinks = [
+  {
+    label: "Home",
+    image: "/icons/Home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    image: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    image: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    image: "/icons/Video.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal Room",
+    image: "/icons/add-personal.svg",
+    route: "/personal-room",
+  },
+];
